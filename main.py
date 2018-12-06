9fb3daf8f579c69c9a4c475c7106d8b796ce654d
KacperKubara <kjk1u17@soton.ac.uk>
2018-12-06 23:40:35 +0000

    Visualisation and age category added

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import importlib
 import pandas as pd 
 import numpy as np 
-import seaborn as sns
 import matplotlib.pyplot as plt
 utils = importlib.import_module('utils')
 
+# Importing the data
 path = '~/Desktop/Machine Learning/Datasets/who_suicide_statistics.csv' 
-
-dataset = utils.get_train_data(path)
+path1 = '~/Desktop/Machine Learning/Datasets/world_population.csv'
+dataset = pd.read_csv(path)
+world_population = pd.read_csv(path1, index_col = 0)
 
 # Check the suicides by gender
 female = dataset[dataset.sex == 'female'].suicides_no.sum(axis = 0)
@@ -24,6 +25,18 @@ for year in timespan:
     deaths_by_year_female.append(dataset[(dataset.year == year) & (dataset.sex == 'female')].suicides_no.sum(axis = 0))
 deaths_by_year = pd.DataFrame({'male': deaths_by_year_male,'female': 
                                deaths_by_year_female }, index = timespan)
-print(deaths_by_year.head())
-deaths_by_year.plot.bar()
+#print(deaths_by_year.head())
+#plt.plot(timespan, deaths_by_year.male,'k',timespan, female_scaled, 'r')
+
 # Sort and display the countries with the biggest suicide rate (suicides_no/population)
+suicide_over_population = deaths_by_year.merge(world_population, left_index=True, right_index=True)
+male_scaled = suicide_over_population.male.divide(suicide_over_population.population)
+female_scaled = suicide_over_population.female.divide(suicide_over_population.population)
+#plt.plot(timespan,male_scaled,'k',timespan, female_scaled, 'r')
+#print(suicide_over_population.head())
+
+# See the suicide distribution across age category
+age_distribution = dataset.drop(columns = ['year', 'country'])
+age_distribution = age_distribution.dropna()
+age_distribution = age_distribution.groupby(by = ['sex','age']).sum()
+print(age_distribution)
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
 import pandas as pd 
 def get_train_data(path):
     return pd.read_csv(path)
+def scale_over_population(suicide_no, world_population):
+    pass
+    
